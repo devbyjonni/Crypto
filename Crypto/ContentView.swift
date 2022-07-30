82c9fa498d6ea24bad875abe9083ead7b814104f
Jonni Åkesson <enterthesource@me.com>
2022-07-30 22:01:16 +0200

    Project setup

---
+++ Crypto/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Crypto
+//
+//  Created by Jonni Akesson on 2022-07-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 13 Pro")
+    }
+}
---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Jonni Akesson on 2022-07-30.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
